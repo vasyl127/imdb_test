54b124f0436795b990b9f4efa6fb766b77ba90f4
Vasyl <vasyl127@gmail.com>
2022-01-31 08:54:31 +0200

    film_controller fix

--- app/controllers/film_controller.rb
+++ app/controllers/film_controller.rb
@@ -1,14 +1,13 @@
 class FilmController < ApplicationController
-  before_action :set_film, only: %i[show]
+  before_action :set_film, only: %i[show rating_update]
   before_action :authenticate_user!, only: %i[rating_update]
 
   def show; end
 
   def rating_update
-    film = set_film
     star = params[:star_id]
-    film.update(rating: rating_formula(film.users_voted, film.rating, star))
-    film.update(users_voted: film.users_voted + 1)
+    @film.update(rating: rating_formula(@film.users_voted, @film.rating, star))
+    @film.update(users_voted: @film.users_voted + 1)
     redirect_to '/'
   end
 
